bb2912d17f081768a7a6cb8cb2510515270d5ad0
Jihad Abdulrazaq <jihad.abdulrazaq@gmail.com>
2022-02-17 22:08:08 +0300

    adding nice inline example create form tailwind

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,6 +5,44 @@
         </h2>
     </x-slot>
 
+    {{-- Form create example --}}
+
+    {{-- <form method="POST">
+        @method('PUT')
+        @csrf
+        <div class="grid grid-cols-2 gap-6">
+            <div class="grid grid-rows-2 gap-6">
+                <div>
+                    <x-label for="name" :value="__('Name')" />
+                    <x-input id="name" class="block mt-1 w-full" type="text" name="name"
+                        value="{{ auth()->user()->name }}" autofocus />
+                </div>
+                <div>
+                    <x-label for="email" :value="__('Email')" />
+                    <x-input id="email" class="block mt-1 w-full" type="email" name="email"
+                        value="{{ auth()->user()->email }}" autofocus />
+                </div>
+            </div>
+            <div class="grid grid-rows-2 gap-6">
+                <div>
+                    <x-label for="new_password" :value="__('New password')" />
+                    <x-input id="new_password" class="block mt-1 w-full" type="password" name="password"
+                        autocomplete="new-password" />
+                </div>
+                <div>
+                    <x-label for="confirm_password" :value="__('Confirm password')" />
+                    <x-input id="confirm_password" class="block mt-1 w-full" type="password" name="password_confirmation"
+                        autocomplete="confirm-password" />
+                </div>
+            </div>
+        </div>
+        <div class="flex items-center justify-end mt-4">
+            <x-button class="ml-3">
+                {{ __('Update') }}
+            </x-button>
+        </div>
+    </form> --}}
+
 
     <x-form-create-validation :errors="$errors" />
 
@@ -18,7 +56,7 @@
             <x-input type="text" placeholder="enter title" class="block mt-1 w-full" name="title" id="title" />
         </div>
 
-        
+
         {{-- Body --}}
         <div>
             <x-label for="body" :value="_('Body')" />
